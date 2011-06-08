8509957b0745c0401d145d2f0546f0757ed485f3
Kári Hreinsson <karihre@gmail.com>
2011-06-08 17:13:46 +0000

    initial

---
+++ xmonad.hs
@@ -0,0 +1,20 @@
+import qualified Data.Map as M
+
+import XMonad
+--import XMonad.Config.Gnome
+import XMonad.Layout.NoBorders
+--import XMonad.Hooks.SetWMName
+
+
+--myWorkspaces = ["Drasl", "Spjall", "Vafr", "Local 1", "Local 2", "Remote 1", "Remote 2", "8", "9"]
+main = do 
+  xmonad $ defaultConfig 
+    { modMask = mod4Mask,
+      terminal = "sakura",
+      keys = \c -> myKeys c `M.union` keys defaultConfig c
+    } 
+  where 
+    myKeys (XConfig {modMask = modm}) = M.fromList $
+      [ ((mod1Mask .|. controlMask, xK_l), spawn "xscreensaver-command -lock")
+      ,  ((mod4Mask, xK_p), spawn "gmrun")
+      ]
